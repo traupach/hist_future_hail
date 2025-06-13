d5499e7e7e8f425e0354f1b6086bdaf75bfaa129
Timothy H. Raupach <timothy.h.raupach@gmail.com>
2025-06-13 13:51:33 +1000

    Added histogram figure to supp mats.

--- scripts/tar_all_directories.sh
+++ scripts/tar_all_directories.sh
@@ -5,7 +5,7 @@
 #PBS -l storage=gdata/up6+gdata/hh5+scratch/up6+massdata/up6
 #PBS -l walltime=10:00:00
 #PBS -l mem=100GB       
-#PBS -l ncpus=40
+#PBS -l ncpus=20
 #PBS -j oe
 #PBS -l wd
 #PBS -W umask=0022
@@ -17,5 +17,4 @@ module load parallel
 # Run from e.g. /g/data/up6/tr2908/hist_future_hail/WRF_v4.4/simulations/remote/
 # And remember to increase ncpus for parallel archiving.
 
-#parallel tar cvf /scratch/up6/tr2908/d03_{= s/\\//_/g =}.tar {}/wrfout_d03* --remove-files ::: */*/WRF
-parallel rsync -arvR {}/wrfout_* /scratch/up6/tr2908/hist_future_hail/remote/ ::: */*/WRF
+parallel tar cvf {}.tar {} ::: *
